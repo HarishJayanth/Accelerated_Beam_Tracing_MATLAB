e4023bfe81be821f68a26931559745003c17b994
HarishJayanth <harish.jayanth127@gmail.com>
2018-11-26 11:31:11 -0500

    Removed local trial files

--- trialFiles/getPathLength.m
+++
@@ -1,8 +0,0 @@
-function pathLen = getPathLength(path)
-
-pathLen = 0;
-for i = 1:size(path,1) - 1
-    pathLen = pathLen + sum((path(i,:) - path(i+1,:)).^2).^0.5;
-end
-
-end
--- trialFiles/sig_interp.m
+++
@@ -1,6 +0,0 @@
-function y = sig_interp(x, fr)
-
-x1 = [x(2:end); 0];
-y = x + fr * (x1 - x);
-
-end
